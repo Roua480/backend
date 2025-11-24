549c4c40afd4d2978441d8febe494114cc5a9771
Roua Hajj Chehade <rhh47@mail.aub.edu>
2025-11-24 08:55:47 +0200

    Update database.py

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-from .core.config import settings
+from core.config import settings
 
 connect_args = (
     {"check_same_thread": False}
