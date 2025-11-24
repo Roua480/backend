78d163372c683ed9728ae581d7192c447c0d5f4f
Roua Hajj Chehade <rhh47@mail.aub.edu>
2025-11-24 08:57:01 +0200

    Update import path for settings in security.py

--- core/security.py
+++ core/security.py
@@ -10,7 +10,7 @@ from sqlalchemy.orm import Session
 from database import get_db
 from models import Session as DBSession
 from models import User, UserRole
-from config import settings
+from core.config import settings
 
 pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
 
